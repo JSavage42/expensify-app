2470ca08f00c30e8007de232969af4f1d2823895
JS06341 <Jason.A.Savage@Cerner.com>
2018-07-30 08:59:41 -0500

    Initial commit for Enterprise

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,3 +10,75 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+const database = firebase.database();
+
+database.ref().set({
+  name: 'Jason Savage',
+  age: 28,
+  stressLevel: 6,
+  job: {
+    title: 'Software Engineer',
+    company: 'Cerner'
+  },
+  location: {
+    city: 'Kansas City',
+    state: 'Kansas',
+    country: 'United State'
+  }
+}).then(() => {
+  console.log('Data is saved');
+
+}).catch((e) => {
+  console.log('This failed.', e);
+});
+
+// database.ref('age').set(29);
+// database.ref('location/city').set('Overland Park');
+
+// database.ref('attributes').set({
+//   height: 69,
+//   weight: 241
+// });
+
+// database.ref('isSingle')
+//   .remove()
+//   .then(() => {
+//     console.log('Data was removed');
+//   }).catch((e) => {
+//     console.log('Did not remove data');
+//   });
+
+// database.ref().update({
+//   stressLevel: 1,
+//   "job/company": 'Cerner',
+//   "location/city": 'Overland Park'
+// })
+//   .then(() => {
+//     console.log('Data is updated');
+//   })
+//   .catch((e) => {
+//     console.log('Data was not updated');
+//   })
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e);
+//   });
+setTimeout(() => {
+  database.ref()
+    .on('value', (snapshot) => {
+      const val = snapshot.val();
+      console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+    });
+}, 3500);
+
+setTimeout(() => {
+  database.ref('name').set('Jess Savage');
+}, 7000);
+
---
+++ src/playground/promises.js
@@ -0,0 +1,18 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve({
+    //   name: 'Jason',
+    //   age: 28
+    reject('Something went wrong.');
+  }, 1500);
+});
+
+console.log('before');
+
+promise.then((data) => {
+  console.log('1', data);
+}).catch((error) => {
+  console.log('Error: ', error);
+});
+
+console.log('after');
