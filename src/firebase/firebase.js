f02e0a630cc2d7db589e6ed01e76e134b92402d2
JS06341 <Jason.A.Savage@Cerner.com>
2018-07-30 09:01:49 -0500

    Initial Enterprise commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,3 +10,33 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+const database = firebase.database();
+
+database.ref('isSingle').remove();
+
+
+// database.ref().set({
+//   name: 'Jason Savage',
+//   age: 28,
+//   isSingle: false,
+//   location: {
+//     city: 'Kansas City',
+//     state: 'Kansas',
+//     country: 'United State'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// database.ref('age').set(29);
+// database.ref('location/city').set('Overland Park');
+
+// database.ref('attributes').set({
+//   height: 69,
+//   weight: 241
+// });
+
