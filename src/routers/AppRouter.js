ac36571957198986eb991932a42e2fd8c4a1ab2a
JS06341 <Jason.A.Savage@Cerner.com>
2018-08-09 08:59:38 -0500

    finalx3

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,6 @@ import createHistory from 'history/createBrowserHistory';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
@@ -13,17 +12,17 @@ import PublicRoute from './PublicRoute';
 export const history = createHistory();
 
 const AppRouter = () => (
-  <Router history={history}>
-    <div>
-      <Switch>
-        <PublicRoute path="/" component={LoginPage} exact={true} />
-        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
-        <PrivateRoute path="/create" component={AddExpensePage} />
-        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </Router>
+    <Router history={history}>
+        <div>
+            <Switch>
+                <PublicRoute path="/" component={LoginPage} exact={true} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </Router>
 );
 
 export default AppRouter;
